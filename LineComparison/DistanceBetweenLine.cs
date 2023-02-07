98cdcad54b299a797e2ed230eefc4efa1a434af0
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 05:24:06 +0530

    Line comparison UC1 program

---
+++ LineComparison/DistanceBetweenLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class DistanceBetweenLine
+    {
+        
+        public static double Distance(double X1, double X2, double Y1, double Y2)
+        {
+            double temp1 = Math.Pow((X2 - X1), 2);
+            double temp2 = Math.Pow((Y2 - Y1), 2);
+            double result = Math.Sqrt(temp1 + temp2);
+            return result;
+        }
+    }
+}
---
+++ LineComparison/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Program");
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:Distance between line UC1");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Please enter X1 and Y1 coordinates");
+                    var X1 = Convert.ToDouble(Console.ReadLine());
+                    var Y1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter X2 and Y2 coordinates");
+                    var X2 = Convert.ToDouble(Console.ReadLine());
+                    var Y2 = Convert.ToDouble(Console.ReadLine());
+                    var finalResult = DistanceBetweenLine.Distance(X1, X2, Y1, Y2);
+
+                    Console.WriteLine("Distance between coordinates {0},{1} and {2},{3} is {4}", X1, Y1, X2, Y2, finalResult);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+            }
+        }
+    }
+}
