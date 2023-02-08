442b80b23d3ba25a40bc094b6838f07ed5ca893e
vaibhav <vaibhavmehta524@gmail.com>
2023-02-08 05:36:55 +0530

    ComparisonBetweenLine program

---
+++ LineComparison/ComparisonBetweenLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class ComparisonBetweenLine
+    {
+        public static void CompareOfLine(double X7, double X8, double Y7, double Y8, double X9, double X10, double Y9, double Y10)
+        {
+            double temp1 = Math.Pow((X8 - X7), 2);
+            double temp2 = Math.Pow((Y8 - Y7), 2);
+            double LengthOfFirstLine = Math.Sqrt(temp1 + temp2);
+
+            double temp3 = Math.Pow((X10 - X9), 2);
+            double temp4 = Math.Pow((Y10 - Y9), 2);
+            double LengthOfSecondLine = Math.Sqrt(temp3 + temp4);
+
+            double Result = LengthOfFirstLine.CompareTo(LengthOfSecondLine);
+            if (Result == 0)
+            {
+                Console.WriteLine("Lenght of two lines are equal");
+            }
+            else if (Result > 0)
+            {
+                Console.WriteLine("Lenght of line1 is greater than length of line2");
+            }
+            else
+            {
+                Console.WriteLine("Length of line1 is smaller than length of line2");
+            }
+        }
+    }
+}
--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -8,7 +8,7 @@ namespace LineComparison
         {
             Console.WriteLine("Welcome to Line Comparison Program");
             Console.WriteLine("Please enter option given below");
-            Console.WriteLine("1:Distance between line UC1\n2:UC2 Check Line equality");
+            Console.WriteLine("1:Distance between line UC1\n2:UC2 Check Line equality\n3:Comparison between line");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -45,6 +45,23 @@ namespace LineComparison
 
                         CheckLineEquality.Equals(X3, Y3, X4, Y4, X5, Y5, X6, Y6);
                     }
+                    break;
+
+                case 3:
+                    //Taking user input
+                    Console.WriteLine("Please enter X7 and Y7 coordinates");
+                    var X7 = Convert.ToDouble(Console.ReadLine());
+                    var Y7 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter X8 and Y8 coordinates");
+                    var X8 = Convert.ToDouble(Console.ReadLine());
+                    var Y8 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter X9 and Y9 coordinates");
+                    var X9 = Convert.ToDouble(Console.ReadLine());
+                    var Y9 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please enter X10 and Y10 coordinates");
+                    var X10 = Convert.ToDouble(Console.ReadLine());
+                    var Y10 = Convert.ToDouble(Console.ReadLine());
+                    ComparisonBetweenLine.CompareOfLine(X7, Y7, X8, Y8, X9, Y9, X10, Y10);
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
